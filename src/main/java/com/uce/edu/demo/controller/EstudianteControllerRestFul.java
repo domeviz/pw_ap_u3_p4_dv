921acd101c99325edf586f2811d753f582148d67
Domenica Vizcarra <domeviz3410@gmail.com>
2023-07-22 10:49:13 -0500

    Taller 32

--- src/main/java/com/uce/edu/demo/controller/EstudianteControllerRestFul.java
+++ src/main/java/com/uce/edu/demo/controller/EstudianteControllerRestFul.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.uce.edu.demo.estudiante.modelo.Estudiante;
@@ -28,11 +30,10 @@ public class EstudianteControllerRestFul {
 	private IEstudianteService estudianteService;
 	
 	//GET
-	@GetMapping(path="/{cedula}")
-	public ResponseEntity<Estudiante> consultarPorCedula(@PathVariable String cedula) {
-		Estudiante estudiante=this.estudianteService.buscarPorCedula(cedula);
-
-		return ResponseEntity.status(227).body(estudiante);
+	@GetMapping(path="/{cedula}",produces=MediaType.APPLICATION_XML_VALUE)
+//	@ResponseStatus(HttpStatus.CONFLICT)
+	public Estudiante consultarPorCedula(@PathVariable String cedula) {
+		return this.estudianteService.buscarPorCedula(cedula);
 	}
 
 	@GetMapping(path="/status/{cedula}")
@@ -42,7 +43,14 @@ public class EstudianteControllerRestFul {
 		return ResponseEntity.status(HttpStatus.OK).body(estudiante);
 	}
 	
-
+	@PostMapping(consumes=MediaType.APPLICATION_JSON_VALUE)
+//	@ResponseStatus(HttpStatus.CONFLICT)
+	public Estudiante guardarYConsultar(@RequestBody Estudiante estudiante) {
+		this.estudianteService.guardar(estudiante);
+		Estudiante e= this.estudianteService.buscarPorID(estudiante.getId());
+		return e;
+	}
+	
 	@GetMapping
 	public ResponseEntity<List<Estudiante>> buscarTodos() {
 		List<Estudiante> estudiantes=this.estudianteService.buscarTodos();
